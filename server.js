e65fe8beadc73b304c00d3f5207bec095cd33720
Hasan Ertas <hsnertas@gmail.com>
2020-07-20 21:12:18 -0400

    heroku

--- server.js
+++ server.js
@@ -10,8 +10,11 @@ app.use(express.json());
 
 app.use(express.static('public'));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', {
-    useNewUrlParser: true
+mongoose.Promise=global.Promise;
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://user:Hme18161816@ds149700.mlab.com:49700/heroku_wpcrwsf6', {
+    useNewUrlParser: true,
+    useMongoClient: true
 });
 
 require('./routes/api-routes')(app);
