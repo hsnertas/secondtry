5f6abdb279d73507118be544485f3f6c5220ac68
Hasan Ertas <hsnertas@gmail.com>
2020-07-20 21:58:22 -0400

    upateds

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,19 +1,18 @@
 const path = require('path');
 
 module.exports = (app) => {
-    app.get('/', (req, res) => {
-        res.sendFile(path.join(__dirname, '../public/index.html'));
+    // app.get('/', (req, res) => {
+    //     res.sendFile(path.join(__dirname, '../public/index.html'));
+    // });
+
+    app.get("/exercise", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/exercise.html"));
+    });
+    app.get("/stats", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/stats.html"));
     });
 
-    app.get('/exercise', (req, res) => {
-        res.sendFile(path.join(__dirname, '../public/exercise.html'));
-    });
-
-    app.get('/stats', (req, res) => {
-        res.sendFile(path.join(__dirname, '../public/stats.html'));
-    });
-
-    app.get('*', (req, res) => {
-        res.redirect('/');
-    });
+    // app.get('*', (req, res) => {
+    //     res.redirect('/');
+    // });
 };
